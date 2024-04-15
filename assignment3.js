99c6b2662ce1a84288ed34677feda5c99197bbb0
Exia <nohitokaze@gmail.com>
2024-04-15 13:52:13 -0400

    3/5 done, halting for now

--- assignment3.js
+++ assignment3.js
@@ -4,35 +4,25 @@ const client = new MongoClient('mongodb://localhost:27017');
 async function run() {
   try {
     // Assignment 3
-    // Grab dataset (boxoffice json) and come up with table and collection convention
-    // Query 1: Find all movies that have a rating > 9.2 & a runtime lower than 100m
-    // Query 2: Find all movies with genre of drama OR action 
+    // Grab dataset (boxoffice json) and come up with table and collection convention (Done)
+    // Query 1: Find all movies that have a rating > 9.2 & a runtime lower than 100m (Done)
+    // Query 2: Find all movies with genre of drama OR action (Done)
     // Query 3: Find all movies with visitors > expectedVisitors
     // Query 4: Use unique operator and query something yourself
     // Query 5: Make custom complex query yourself
     // Do this all via nodejs driver compare code
     console.log('in');
-    const database = client.db('exercise2');
-    const fighters = database.collection('champions');
-    // console.log(movies);
-    // Query for a movie that has the title 'Back to the Future'
-    const queryToInsert1 = { _id: 'KI', name: 'Jago' , style: 'kung-fu'};
-    const queryToInsertMany = [
-      {_id: 'SF', name: 'Ken', style: 'shoto'}, 
-      {_id: 'STRIDER', name: 'hiryu', style: 'ninjutsu'}, 
-      {_id: 'MK', name: 'liu kang', style: 'shaolin'}
-    ];
-    const queryToInsertManyWithConflict = [
-      {_id: 'SF', name: 'ryu', style: 'shoto'},
-      {_id: 'MK', name: 'kung lao', style: 'shaolin'},
-      {_id: 'tekken', name: 'jin', style: 'karate'}
-    ];
-    const queryWithJournal = {
-      _id: 'maverick hunter',
-      name: 'zero',
-      style: 'saber'
-    };
-    const resp = await fighters.insertOne(queryWithJournal, {w: 1, j: true});
+    const database = client.db('box_office');
+    const movies = database.collection('movies');
+
+    const query1 = {$and: [{"meta.rating": {$gt: 9.2}},{"meta.runtime": {$lt: 100}}]};
+    const query2 = {genre: {$in: ['drama', 'action']}};
+    const query3 = {};
+    const query4 = {};
+    const query5 = {};
+
+
+    const resp = await movies.find(query2).toArray();
     console.log(resp);
   } catch(e) {
     console.error('Failed somehow')
