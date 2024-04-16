e622de197ebf7084dccf8c301c271b361160d96e
Exia <nohitokaze@gmail.com>
2024-04-15 23:08:39 -0400

    Adding finished assignment 3

--- assignment3.js
+++ assignment3.js
@@ -7,22 +7,18 @@ async function run() {
     // Grab dataset (boxoffice json) and come up with table and collection convention (Done)
     // Query 1: Find all movies that have a rating > 9.2 & a runtime lower than 100m (Done)
     // Query 2: Find all movies with genre of drama OR action (Done)
-    // Query 3: Find all movies with visitors > expectedVisitors
-    // Query 4: Use unique operator and query something yourself
-    // Query 5: Make custom complex query yourself
-    // Do this all via nodejs driver compare code
+    // Query 3: Find all movies with visitors > expectedVisitors(Done)
     console.log('in');
     const database = client.db('box_office');
     const movies = database.collection('movies');
 
     const query1 = {$and: [{"meta.rating": {$gt: 9.2}},{"meta.runtime": {$lt: 100}}]};
     const query2 = {genre: {$in: ['drama', 'action']}};
-    const query3 = {};
-    const query4 = {};
-    const query5 = {};
+    const query3 = {$expr: {$gt: ['$visitors', '$expectedVisitors']}};
 
 
-    const resp = await movies.find(query2).toArray();
+
+    const resp = await movies.find(query3).toArray();
     console.log(resp);
   } catch(e) {
     console.error('Failed somehow')
